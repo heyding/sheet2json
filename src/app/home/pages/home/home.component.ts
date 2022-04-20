5527f1f9b754633facd64ecef3d6659c33e26f52
Tommy Heyding <toh@hbt.de>
2022-04-20 17:09:17 +0200

    Add github link

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -1,8 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {Store} from '@ngrx/store';
-import {HomeSelectors} from './store/home.selectors';
-import {HomeActions} from './store/home.actions';
 import {JsonService} from '../../../shared/services/json.service';
 
 
@@ -13,37 +9,15 @@ import {JsonService} from '../../../shared/services/json.service';
 })
 export class HomeComponent implements OnInit {
 
-  userInput$: Observable<string> | undefined;
-  userInput: string | undefined;
-  displayUserInput: boolean = false;
-
   sheetID: string | undefined;
   sheetName: string | undefined;
 
-
-  constructor(private store: Store, private jsonDownloadService: JsonService) {
+  constructor(private jsonDownloadService: JsonService) {
   }
 
   ngOnInit(): void {
-    this.userInput$ = this.store.select(HomeSelectors.selectUserInput);
     this.sheetID = '';
     this.sheetName = '';
-  }
-
-  storeUserInput(userInput: string) {
-    this.displayUserInput = true;
-    this.store.dispatch(HomeActions.setUserInput({userInput}));
-    this.userInput = '';
-  }
-
-  resetStore() {
-    this.displayUserInput = false;
-    this.store.dispatch(HomeActions.setUserInput({userInput: 'Hello world!'}));
-    this.userInput = '';
-  }
-
-  hideUserInput(): void {
-    this.displayUserInput = false;
   }
 
   previewJSON(url: string, sheetName: string) {
@@ -62,9 +36,6 @@ export class HomeComponent implements OnInit {
 
     // Extract the sheetID which is a string inside the URL
     url = url.replace(prefix, '');
-    const sheetID = url.split(suffix)[0];
-    return sheetID;
+    return url.split(suffix)[0];
   }
-
-
 }
