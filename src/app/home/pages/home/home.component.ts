0bf4522ddc446ab5f414046a394426996383acef
Tommy Heyding <toh@hbt.de>
2022-04-20 11:13:14 +0200

    Use api to fetch data from sheet and store it into json file

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -3,6 +3,9 @@ import {Observable} from 'rxjs';
 import {Store} from '@ngrx/store';
 import {HomeSelectors} from './store/home.selectors';
 import {HomeActions} from './store/home.actions';
+import {JsonService} from '../../../shared/services/json.service';
+
+// import FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-home',
@@ -15,11 +18,18 @@ export class HomeComponent implements OnInit {
   userInput: string | undefined;
   displayUserInput: boolean = false;
 
-  constructor(private store: Store) {
+  sheetID: string | undefined;
+  sheetName: string | undefined;
+
+  apiBaseUrl = 'https://opensheet.elk.sh/';
+
+  constructor(private store: Store, private jsonDownloadService: JsonService) {
   }
 
   ngOnInit(): void {
     this.userInput$ = this.store.select(HomeSelectors.selectUserInput);
+    this.sheetID = '1k0ETCI_rPqrPHrnlcdgWfU4NU7Pfx4fUuVkhZhZxwfc';
+    this.sheetName = 'family';
   }
 
   storeUserInput(userInput: string) {
@@ -37,4 +47,20 @@ export class HomeComponent implements OnInit {
   hideUserInput(): void {
     this.displayUserInput = false;
   }
+
+  /*  private fetchSheetDataAsJSON(sheetID: string, sheetName: string): void {
+      this.http.get(this.apiBaseUrl.concat('/').concat(sheetID).concat('/').concat(sheetName)).subscribe(response => {
+        console.log(response);
+        // this.downloadFile(response);
+      })};*/
+
+  previewJSON(sheetID: string, sheetName: string) {
+    this.jsonDownloadService.previewJSON(sheetID, sheetName);
+  }
+
+  downloadJSON(sheetID: string, sheetName: string) {
+    this.jsonDownloadService.downloadJSON(sheetID, sheetName);
+  }
+
+
 }
