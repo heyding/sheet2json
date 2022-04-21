c378b3114737655d862516a8be5507ffa2514e74
Tommy Heyding <toh@hbt.de>
2022-04-21 13:11:46 +0200

    Add banner with api reference

--- src/app/home/components/api-reference/api-reference.component.ts
+++ src/app/home/components/api-reference/api-reference.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-api-reference',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApiReferenceComponent implements OnInit {
 
-  constructor() { }
+  displayBanner: boolean = true;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  dismissBanner(): void {
+    this.displayBanner = false;
   }
 
 }
